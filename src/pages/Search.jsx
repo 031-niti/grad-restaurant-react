a4605ba2443e4af5eb6cba35622931810d1d54a7
031-niti <earthza6263@gmail.com>
2023-09-25 09:48:05 +0700

    edit

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 import { Link } from 'react-router-dom'
 
@@ -13,23 +13,48 @@ const config = {
 };
 
 const Search = () => {
-  const [search, setSearch] = useState();
+  const [restaurants, setRestaurants] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
+  const [error, setError] = useState(false);
+
+  const handleSearchTermChange = (e) => {
+    setSearchTerm(e.target.value);
+  };
 
   const handleSearch = async (e) => {
     e.preventDefault();
     try {
-       
+      const response = await axios.get(`${url}/restaurants`, {
+        params: {
+        search: searchTerm,
+        },
+      });
+      setRestaurants(response.data);
     } catch (error) {
-        console.log(error);
+      console.error(error);
+      setError(true);
     }
-};
+  };
+
+ useEffect(() => {
+ }, []);
   
   return (
     <div className="search w-50 mx-auto">
       <form className="d-flex" role="search">
-        <input className="form-control me-2" name="search" placeholder="Search" aria-label="Search" />
+        <input className="form-control me-2" name="search" placeholder="Search" aria-label="Search" onClick={handleSearchTermChange}/>
         <Link className="btn btn-outline-success" onClick={handleSearch}>Search</Link>
       </form>
+      {error && <p>Error occurred while searching.</p>}
+      <ul>
+            {restaurants.map((restaurant) => (
+              <li key={restaurant.id}>
+                <Link to={`/restaurant/${restaurant.id}`}>
+                  {restaurant.name} - {restaurant.type}
+                </Link>
+              </li>
+            ))}
+          </ul>
     </div>  
   )
 }
